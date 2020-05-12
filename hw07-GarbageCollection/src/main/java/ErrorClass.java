4c7773312a459a88cc02b103f85ffa619d187536
katerina-creator <chistova.ekaterina83@gmail.com>
2020-05-12 21:28:25 +0300

    hw07-GarbageCollection

--- hw07-GarbageCollection/src/main/java/ErrorClass.java
+++ hw07-GarbageCollection/src/main/java/ErrorClass.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ErrorClass {
+
     public static void main(String[] args) throws InterruptedException {
         List<Long> list = new ArrayList<>();
             long i = 1000;
