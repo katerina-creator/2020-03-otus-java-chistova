8a08cc83f7fcf4cab74cb9ca1f347fd31bd2e31c
katerina-creator <chistova.ekaterina83@gmail.com>
2020-12-18 01:18:41 +0300

    HW15-TODO

--- hw15-TODO/src/main/java/ru/otus/listener/homework/State.java
+++ hw15-TODO/src/main/java/ru/otus/listener/homework/State.java
@@ -6,21 +6,29 @@ import java.util.LinkedList;
 
 /** хранение состояния сообщения **/
 public class State {
-    private final LinkedList<Message> listMsg;
+    private final Message oldMsg;
+    private final Message newMsg;
 
     State(State state) {
-        listMsg = state.listMsg;
+        oldMsg = state.oldMsg;
+        oldMsg.toBuilder().field13(state.oldMsg.getField13()).build();
+        newMsg = state.newMsg;
+        newMsg.toBuilder().field13(state.newMsg.getField13()).build();
     }
 
     public State(Message oldMsg, Message newMsg) {
-        listMsg = new LinkedList<>();
-        listMsg.add(oldMsg);
-        listMsg.add(newMsg);
+        this.oldMsg=oldMsg;
+        oldMsg.toBuilder().field13(oldMsg.getField13()).build();
+        this.newMsg=newMsg;
+        newMsg.toBuilder().field13(newMsg.getField13()).build();
+
     }
 
     @Override
     public String toString() {
-        return ": " + (listMsg == null ? null :
-                     listMsg.toString()) ;
+        return ": oldMsg: " + (oldMsg == null ? null :
+                oldMsg.toString()) +
+                "   newMsg: " + (newMsg == null ? null :
+                newMsg.toString());
     }
 }
--- hw15-TODO/src/main/java/ru/otus/processor/homework/ProcessorChange11and12Fields.java
+++ hw15-TODO/src/main/java/ru/otus/processor/homework/ProcessorChange11and12Fields.java
@@ -10,8 +10,8 @@ public class ProcessorChange11and12Fields  implements Processor {
         Message newMessage = new Message.Builder(1L)
                 .field11(message.getField12())
                 .field12(message.getField11())
+                .field13(message.getField13())
                 .build();
-
         return newMessage;
     }
 }
