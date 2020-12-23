57c40ea07481eb62d17ed37c801143feed8059ef
katerina-creator <chistova.ekaterina83@gmail.com>
2020-12-23 21:18:55 +0300

    HW15-TODO - правка ProcessorErrorTest, Message, State

--- hw15-TODO/src/main/java/ru/otus/listener/homework/State.java
+++ hw15-TODO/src/main/java/ru/otus/listener/homework/State.java
@@ -11,55 +11,15 @@ public class State {
     private Message newMsg;
 
     State(State state) {
-        makeCopy(state.oldMsg, state.newMsg);
+        this.oldMsg = state.oldMsg.clone();
+        this.newMsg = state.newMsg.clone();
     }
 
     public State(Message oldMsg, Message newMsg) {
-        makeCopy(oldMsg, newMsg);
+        this.oldMsg = oldMsg.clone();
+        this.newMsg = newMsg.clone();
     }
 
-
-    public void makeCopy(Message oldMsg, Message newMsg) {
-        ObjectForMessage oldMsgObject = new ObjectForMessage();
-        if (oldMsg.getField13()!=null) {
-            oldMsgObject.setData(oldMsg.getField13().getData());
-        }
-        this.oldMsg = new Message.Builder(1L).field1(oldMsg.getField1())
-                .field2(oldMsg.getField2())
-                .field3(oldMsg.getField3())
-                .field4(oldMsg.getField4())
-                .field5(oldMsg.getField5())
-                .field6(oldMsg.getField6())
-                .field7(oldMsg.getField7())
-                .field8(oldMsg.getField8())
-                .field9(oldMsg.getField9())
-                .field10(oldMsg.getField10())
-                .field11(oldMsg.getField11())
-                .field12(oldMsg.getField12())
-                .field13(oldMsgObject)
-                .build();
-
-        ObjectForMessage newMsgObject = new ObjectForMessage();
-        if (newMsg.getField13()!=null) {
-            newMsgObject.setData(newMsg.getField13().getData());
-        }
-
-        this.newMsg = new  Message.Builder(1L).field1(newMsg.getField1())
-                .field2(newMsg.getField2())
-                .field3(newMsg.getField3())
-                .field4(newMsg.getField4())
-                .field5(newMsg.getField5())
-                .field6(newMsg.getField6())
-                .field7(newMsg.getField7())
-                .field8(newMsg.getField8())
-                .field9(newMsg.getField9())
-                .field10(newMsg.getField10())
-                .field11(newMsg.getField11())
-                .field12(newMsg.getField12())
-                .field13(newMsgObject)
-                .build();
-
-    }
     @Override
     public String toString() {
         return ":   oldMsg: " + (oldMsg == null ? null :
